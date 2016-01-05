d57675eab194fe2d879b163081f68cb72a28c929
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-01-05 09:00:10 -0800

    Day One Portfolio ---------------- Basic HTML framework Project object constructor function to dynamically add new projects to page

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 var allProjects = [];
 
-function Project(info){
-  this.title = info.title;
-  this.picture = info.picture;
-  this.link = info.link;
-}
+function Project(features){
+  // this.self = this;
+  this.title = features.title;
+  this.picture = features.picture;
+  this.link = features.link;
+  this.display = function(){
+    var $newproject = $('article.template').clone();
+
+    $newproject.data('title', this.title);
+    $newproject.data('picture', this.picture);
+    $newproject.data('link', this.link);
+
+
+    $newproject.find('a').attr('href', this.link);
+    $newproject.find('img').attr('src', this.picture);
+    $newproject.removeClass('template');
+    return $newproject;
+  };
+};
+
+var rawData = [
+  {
+    title: 'Tic Tac Toe',
+    picture: 'img/tictactoe.png',
+    link: ''
+  }
+];
+
+
+rawData.forEach(function(projInd) {
+  allProjects.push(new Project(projInd));
+});
+
+allProjects.forEach(function(a){
+  $('#projects').append(a.display());
+});
