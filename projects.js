1cfb4846a5d9678e85c086784d9f1d5b3debfc52
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-01-08 07:54:41 -0800

    responsive layout, change of orientation of projects in mobile vs. desktop

--- projects.js
+++ projects.js
@@ -3,5 +3,30 @@ var rawData = [
     title: 'Tic Tac Toe',
     picture: 'img/tictactoe.png',
     link: ''
+  },
+  {
+    title: 'Tic Tac Toe',
+    picture: 'img/tictactoe.png',
+    link: ''
+  },
+  {
+    title: 'Tic Tac Toe',
+    picture: 'img/tictactoe.png',
+    link: ''
+  },
+  {
+    title: 'Tic Tac Toe',
+    picture: 'img/tictactoe.png',
+    link: ''
+  },
+  {
+    title: 'Tic Tac Toe',
+    picture: 'img/tictactoe.png',
+    link: ''
+  },
+  {
+    title: 'Tic Tac Toe',
+    picture: 'img/tictactoe.png',
+    link: ''
   }
 ];
